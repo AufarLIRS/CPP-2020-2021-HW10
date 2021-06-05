08534c5b53f5b30a1bc231be2cc95e4933df8c29
Alaedindouak <alaedin.hak@gmail.com>
2021-06-06 01:04:36 +0300

    hw_10

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QCheckBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_01_clicked();
+
+    void on_pushButton_02_clicked();
+
+    void on_pushButton_03_clicked();
+
+    void on_pushButton_04_clicked();
+
+    void on_reset_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    static constexpr int button_number = 4;
+    static constexpr int sequece[] = {3, 4, 1, 2};
+
+    int buttons_pressed_right = 0;
+    void reset_ui();
+    void block_ui();
+    inline bool check_win();
+    void proccess_button(QPushButton*, QCheckBox*);
 };
 #endif // MAINWINDOW_H
